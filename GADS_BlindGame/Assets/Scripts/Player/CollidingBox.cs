7274cb80e7bac4125dd0d1317b65145ae2f3e067
Crazy24699 <110395759+Crazy24699@users.noreply.github.com>
2024-04-04 22:12:56 +0200

    spawning works

--- GADS_BlindGame/Assets/Scripts/Player/CollidingBox.cs
+++ GADS_BlindGame/Assets/Scripts/Player/CollidingBox.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class CollidingBox : MonoBehaviour
 {
-
+    public GameObject PointRef;
 
     public InteractedObjects CurrentObject;
 
@@ -25,39 +25,96 @@ public class CollidingBox : MonoBehaviour
 
     public void OnTriggerStay(Collider ObjectCollision)
     {
-        
+
+
+
+        //RetrievePointData(ObjectCollision);
+        PointDataTest(ObjectCollision);
+        if (CurrentObject != null)
+        {
+            //Debug.Log(CurrentObject.FoundVertices.Count);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        CurrentObject.UpdateVectorDraws = false;
     }
 
     public void RetrievePointData(Collider CollisionRef)
     {
         if (!CollisionRef.CompareTag("Interactable"))
         {
+            Debug.Log("Clock");
             return;
         }
 
-        MeshFilter MeshRef = CollisionRef.GetComponent<MeshFilter>();
 
-        CurrentObject = CollisionRef.TryGetComponent(out CurrentObject) ?
-        CollisionRef.GetComponent<InteractedObjects>() :
-        CollisionRef.AddComponent<InteractedObjects>();
+        Debug.Log("found    ");
 
-        if((MeshRef != null && CurrentObject != null) && !CurrentObject.FoundAll)
+        //CurrentObject = CollisionRef.TryGetComponent(out CurrentObject) ?
+        //CollisionRef.GetComponent<InteractedObjects>() :
+        //CurrentObject=CollisionRef.AddComponent<InteractedObjects>();
+
+        CurrentObject = CollisionRef.gameObject.GetComponent<InteractedObjects>();
+        Mesh MeshRef = CollisionRef.GetComponent<MeshFilter>().sharedMesh;
+        HashSet<Vector3> Verts = new HashSet<Vector3>(MeshRef.vertices);
+
+
+        if((MeshRef != null && CurrentObject != null))
         {
             Transform WorldTransform = CollisionRef.transform;
-            Vector3[] AllVertices = MeshRef.sharedMesh.vertices.ToArrayPooled();
+            //Vector3[] AllVertices = MeshRef.sharedMesh.vertices.ToArrayPooled();
+            List<Vector3> Coords = new List<Vector3>(Verts);
 
-            for (int i = 0; i < AllVertices.Length; i++)
+            for (int i = 0; i < Coords.Count; i++)
             {
-                Vector3 VertWorldPosition = transform.TransformPoint(AllVertices[i]);
+                Vector3 VertWorldPosition = Coords[i];
                 if (this.GetComponent<Collider>().bounds.Contains(VertWorldPosition))
                 {
-                    CurrentObject.FoundVertices.Add(VertWorldPosition);
+                    CurrentObject.FoundVertices.Add(WorldTransform.position);
                 }
             }
 
         }
-        
+        CurrentObject.UpdateVectorDraws = true;
+
     }
 
+
+    public void PointDataTest(Collider CollisionRef)
+    {
+        if (!CollisionRef.CompareTag("Interactable"))
+        {
+            Debug.Log("Clock");
+            return;
+        }
+
+        Debug.Log("found    ");
+        CurrentObject = CollisionRef.TryGetComponent(out CurrentObject) ?
+        CollisionRef.GetComponent<InteractedObjects>() :
+        CurrentObject = CollisionRef.AddComponent<InteractedObjects>();
+
+        Mesh MeshRef =CollisionRef.GetComponent<MeshFilter>().sharedMesh;
+        Vector3[] AllVerticies = MeshRef.vertices;
+
+        Transform WorldVertexTransform = CollisionRef.GetComponent<MeshFilter>().transform;
+
+        if (MeshRef != null)
+        {
+
+            for (int i = 0; i < AllVerticies.Length; i++)
+            {
+                Vector3 VertexWorldPosition = WorldVertexTransform.TransformPoint(AllVerticies[i]);
+                if(this.GetComponent<Collider>().bounds.Contains(VertexWorldPosition)
+                && !CurrentObject.FoundVertices.Contains(VertexWorldPosition))
+                {
+                    Instantiate(PointRef, VertexWorldPosition, Quaternion.identity);
+                    CurrentObject.FoundVertices.Add(VertexWorldPosition);
+                }
+            }
+        }
+
+    }
 
 }
--- GADS_BlindGame/Assets/Scripts/World Functionality/Objects/InteractedObjects.cs
+++ GADS_BlindGame/Assets/Scripts/World Functionality/Objects/InteractedObjects.cs
@@ -3,23 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-[ExecuteInEditMode]
+//[ExecuteInEditMode]
 public class InteractedObjects : MonoBehaviour
 {
-    public HashSet<Vector3> FoundVertices = new HashSet<Vector3>();
+    //public HashSet<Vector3> FoundVertices = new HashSet<Vector3>();
+    public List<Vector3> FoundVertices = new List<Vector3>();
     protected HashSet<Vector3> AllVertices = new HashSet<Vector3>();
 
     [HideInInspector]public bool FoundAll = false;
-    
+    public bool UpdateVectorDraws;
 
     // Start is called before the first frame update
     void Start()
     {
+        
         if(FoundAll)
         {
             FoundAll = false;
         }
-        AllVertices = this.gameObject.GetComponent<MeshFilter>().sharedMesh.vertices.ToHashSet();
+        //FoundVertices.Add(Vector3.zero);
+        //AllVertices = this.gameObject.GetComponent<MeshFilter>().sharedMesh.vertices.ToHashSet();
         //Debug.Log(AllVertices.Count);
     }
 
@@ -27,16 +30,26 @@ public class InteractedObjects : MonoBehaviour
     protected void DrawObjectData()
     {
 
+        foreach(var Vertex in FoundVertices)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(Vertex, 0.1f);
+            Debug.Log("matter   "+ AllVertices.Count);
+        }
     }
 
     private void OnDrawGizmos()
     {
-        if(FoundVertices.Count==AllVertices.Count)
-        {
-            return;
-        }
+        //if(FoundVertices.Count==AllVertices.Count && !UpdateVectorDraws)
+        //{
+        //    return;
+        //}
 
+        //if (UpdateVectorDraws)
+        //{
+        //    DrawObjectData();
 
+        //}
 
     }
 
