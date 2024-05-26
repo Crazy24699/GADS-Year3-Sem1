b0719d59f1aa696ab11f267a07c46e7621a7c2c6
Crazy24699 <110395759+Crazy24699@users.noreply.github.com>
2024-05-26 20:42:35 +0200

    Switching scenes works

--- GADS_BlindGame/Assets/ProgramManager.cs
+++ GADS_BlindGame/Assets/ProgramManager.cs
@@ -1,6 +1,9 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ProgramManager : MonoBehaviour
 {
@@ -8,10 +11,14 @@ public class ProgramManager : MonoBehaviour
     public BlindLevel CurrentBlindness;
     public static ProgramManager ManagerInstance;
 
-    public List<int> Sequence1 = new List<int>();
+    public List<int> Sequence1;
     public List<int> Sequence2 = new List<int>();
     public List<int> Sequence3 = new List<int>();
 
+    public List<int> CurrentSequence = new List<int>();
+    [SerializeField]protected int CurrentSequenceList = 0;
+
+    bool Finished = false;
 
     // Start is called before the first frame update
     void Start()
@@ -25,10 +32,74 @@ public class ProgramManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+        SetNewSequence();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            LoadNextLevel();
+        }
+        if(CurrentSequenceList>=3 && CurrentSequence.Count == 0)
+        {
+            Finished = true;
+        }
+    }
 
+    public void SetNewSequence()
+    {
+        if (CurrentSequenceList >= 3)
+        {
+            SceneManager.LoadScene("Main Menu");
+            return;
+        }
+        switch (CurrentSequenceList)
+        {
+            case 0:
+                CurrentSequence = Sequence1.ToList();
+                break;
 
+            case 1:
+                CurrentSequence = Sequence2.ToList();
+                break;
+
+            case 2:
+                CurrentSequence = Sequence3.ToList();
+                break;
+        }
+
+        CurrentSequenceList++;
+    }
+
+    public void LoadNextLevel()
+    {
+        if (CurrentSequence.Count <= 0)
+        {
+            SetNewSequence();
+        }
+        if(CurrentSequenceList > 3 || Finished)
+        {
+            return;
+        }
+        int LevelNum = CurrentSequence[0];
+        switch (LevelNum)
+        {
+            case 0:
+                SceneManager.LoadScene("Cement Level");
+                break;
+
+            case 1:
+                SceneManager.LoadScene("Hammer Level");
+                break;
+
+            case 2:
+                SceneManager.LoadScene("Crane Level");
+                break;
+        }
+        CurrentSequence.RemoveAt(0);
+    }
+    
 }
 
 public enum LevelType
